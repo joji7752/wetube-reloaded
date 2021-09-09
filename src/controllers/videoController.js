4fb45d3cbb0e375a8d0236c1bc06d61c80629054
yeongji Cho <joji7752@gmail.com>
2021-09-09 20:31:23 +0900

    feat: add delete, search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ import Video from "../models/Video";
 
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}).sort({ createdAt: "desc" });
     return res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     return res.render("server-error", error);
@@ -71,4 +71,30 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  try {
+    //delete video
+    await Video.findByIdAndDelete(id);
+    return res.redirect("/");
+  } catch (error) {
+    console.log(`error====>`, error._message);
+  }
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    //search
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`^${keyword}`, "i"),
+        //$gt: 3 greater than3,3보다 많은
+      },
+    }).sort({ createdAt: "desc" });
+  }
+  return res.render("search", { pageTitle: "👁‍🗨 Search Video", videos });
 };
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -13,6 +14,7 @@ const videoRouter = express.Router();
 //([0-9a-f]{24}) mongodb의 자동생성 objectID의 조건(24byte hexadecimal string = 24바이트 16진수)에 맞는 값만 받는 정규식 - objectID는 0부터9 + a부터f 까지인 16진수 24자 string로 이뤄져있음,
 videoRouter.route("/:id([0-9a-f]{24})").get(watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
